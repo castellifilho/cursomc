a90d3a8bd010e24a110fc3e03105b93f97f514e9
Castelli <isisdare@gmail.com>
2020-03-19 00:33:08 -0300

    Categoria: repository e service

---
+++ src/main/java/com/castelli/cursomc/services/CategoriaService.java
@@ -0,0 +1,21 @@
+package com.castelli.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.castelli.cursomc.domain.Categoria;
+import com.castelli.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+}
