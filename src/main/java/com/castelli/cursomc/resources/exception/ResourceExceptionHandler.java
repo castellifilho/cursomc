27f0d268949959172df58846703cc2c905ce5a58
Castelli <isisdare@gmail.com>
2020-03-26 02:20:38 -0300

    CRUD em Categoria

--- src/main/java/com/castelli/cursomc/resources/exception/ResourceExceptionHandler.java
+++ src/main/java/com/castelli/cursomc/resources/exception/ResourceExceptionHandler.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import com.castelli.cursomc.services.exceptions.DataIntegrityException;
 import com.castelli.cursomc.services.exceptions.ObjectNotFoundException;
 
 @ControllerAdvice
@@ -14,5 +15,11 @@ public class ResourceExceptionHandler {
 	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest reuest){
 		StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+	}
+	
+	@ExceptionHandler(DataIntegrityException.class)
+	public ResponseEntity<StandardError> dataIntegrity(DataIntegrityException e, HttpServletRequest reuest){
+		StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
 	}
 }
--- src/main/java/com/castelli/cursomc/services/CategoriaService.java
+++ src/main/java/com/castelli/cursomc/services/CategoriaService.java
@@ -2,9 +2,11 @@ package com.castelli.cursomc.services;
 
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 import com.castelli.cursomc.domain.Categoria;
 import com.castelli.cursomc.repositories.CategoriaRepository;
+import com.castelli.cursomc.services.exceptions.DataIntegrityException;
 import com.castelli.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -13,9 +15,28 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository repo;
 	
-	public Categoria buscar(Integer id) {
+	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 			"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+	}
+	
+	public Categoria insert(Categoria obj) {
+		obj.setId(null);
+		return repo.save(obj);
+	}
+	
+	public Categoria update(Categoria obj) {
+		find(obj.getId());
+		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		}catch(DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possivel excluir uma categoria que possui produtos");
+		}				
 	}
 }
